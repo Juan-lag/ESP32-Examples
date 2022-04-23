eb8c1d2e3ddcb3e7740ea8c060e8446c9e2b70c2
Juan-lag <celuar12@gmail.com>
2022-04-22 23:27:27 -0500

    se agregó la red

--- 20_UDP/config.h
+++ 20_UDP/config.h
@@ -1,5 +1,5 @@
-const char* ssid     = "ssid";
-const char* password = "password";
+const char* ssid = "Pacme 2.4";  // nombre de la red
+const char* password = "Yurolo25ev";
 const char* hostname = "ESP32_1";
 
 IPAddress ip(192, 168, 1, 200);
